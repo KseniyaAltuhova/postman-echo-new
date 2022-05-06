7c29dc86cce5ef8a5f283c9a6612fbd6a7bf15e0
kseniyaaltuhova <bogomolowakseniya@yandex.ru>
2022-05-06 17:44:32 +0300

    add postman echo test

---
+++ src/test/java/ru/netology/echo/PostmanEchoTest.java
@@ -0,0 +1,43 @@
+package ru.netology.echo;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldTestPostValue() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello! Can i help you?")
+                .log().all()
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello! Can i help you?"))
+                .contentType(ContentType.JSON)
+                .log().all()
+        ;
+    }
+
+    @Test
+    void shouldTestPostValueRu() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Здравствуйте! Чем я могу Вам помочь?")
+
+                .log().all()
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .body("data", equalTo("Здравствуйте! Чем я могу Вам помочь?"))
+                .contentType(ContentType.JSON)
+                .log().all()
+        ;
+    }
+}
